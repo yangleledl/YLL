1cef2d6c5fff012de6a4293e89757aa5123cc5e0
denglong@exdl.cn <denglong@exdl.cn>
2020-04-16 15:17:53 +0800

    这是注释啊啊啊啊

---
+++ src/cn/hello/idea/DemoGit.java
@@ -0,0 +1,7 @@
+package cn.hello.idea;
+
+public class DemoGit {
+    public static void main(String args[]){
+        System.out.println("jfjfjfjfj");
+    }
+}
---
+++ src/cn/hello/idea/HelloWorld.java
@@ -0,0 +1,9 @@
+package cn.hello.idea;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+       System.out.print("helloworld");
+
+    }
+}
